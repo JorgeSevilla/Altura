344a908acf1a577fe2fb56dabca692dd3723bdd6
JorgeSevilla <solopunks803@gmail.com>
2017-11-16 17:10:42 -0200

    Expressao regular e join

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
+import re
 
 
 def cadastrar(nomes):  # Cadastrar nomes novos
@@ -39,12 +40,21 @@ def procurar(nomes):  # Procurar o nome cadastrado
         print('Nome %s ñao está cadastrado' % nome_a_procurar)
 
 
+def procurar_regex(nomes):  # Expressão regular e join
+    print('Digite a expressão regular')
+    regex = input()
+    nomes_concatenados = ' '.join(nomes)
+    resulados = re.findall(regex, nomes_concatenados)
+    print(resulados)
+
+
 def menu():  # Menú para escoger la opción a ejecutar
     nomes = []
     escolha = ''
     while escolha != '0':
-        print('Digite 1 para cadastrar, 2 para ver nomes, 3 para remover nomes,'
-              ' 4 para alterar nome, 5 para procurar un nome, 0 para terminar')
+        print('1 Para cadastrar.\n2 Para ver nomes.\n3 Para remover nomes.\n'
+              '4 Para alterar nome.\n5 Para procurar un nome.\n'
+              '6 Busque pela expressão regular. \n0 Para terminar.\n')
         escolha = input()
 
         if escolha == '1':
@@ -57,5 +67,7 @@ def menu():  # Menú para escoger la opción a ejecutar
             alterar(nomes)
         elif escolha == '5':
             procurar(nomes)
+        elif escolha == '6':
+            procurar_regex(nomes)
 
 menu()
